269b9975c218b2c0bbc55909c9acf4edd12578ed
Yauri Attamimi <yauritux@gmail.com>
2023-05-22 17:51:01 +0700

    implement user document entity mapper and user document repository adapters

--- phone-booking-restws/src/main/java/link/yauritux/phonebookingrestws/adapter/output/persistence/mapper/UserDocumentEntityMapper.java
+++ phone-booking-restws/src/main/java/link/yauritux/phonebookingrestws/adapter/output/persistence/mapper/UserDocumentEntityMapper.java
@@ -1,2 +1,48 @@
-package link.yauritux.phonebookingrestws.adapter.output.persistence.mapper;public class UserDocumentEntityMapper {
+package link.yauritux.phonebookingrestws.adapter.output.persistence.mapper;
+
+import link.yauritux.domain.entity.User;
+import link.yauritux.phonebookingrestws.adapter.output.persistence.document.UserDocument;
+
+/**
+ * @author Yauri Attamimi (yauritux@gmail.com)
+ * @version 1.0
+ *
+ * A mapper class to convert a user document collection to a user domain entity, and vice versa.
+ *
+ * @see link.yauritux.phonebookingrestws.adapter.output.persistence.document.UserDocument
+ * @see link.yauritux.domain.entity.User
+ */
+public class UserDocumentEntityMapper {
+
+    public static final UserDocumentEntityMapper INSTANCE = new UserDocumentEntityMapper();
+
+    private UserDocumentEntityMapper() {}
+
+    /**
+     * Convert user entity to its corresponding user document collection in mongo.
+     * This is used as a necessary step before saving a user record into as collection in our mongo database.
+     *
+     * @param user a user entity as defined within the core domain
+     * @return UserDocument
+     */
+    public UserDocument entityToDocument(User user) {
+        return UserDocument.builder()
+                .name(user.getName())
+                .build();
+    }
+
+    /**
+     * Convert a user document to its corresponding user entity.
+     * This is used after we fetched a user collection from mongo and proceed with further logics
+     * defined in our core domain layer.
+     *
+     * @param userDocument a user document that represents a user collection in mongo database.
+     * @return User
+     */
+    public User documentToEntity(UserDocument userDocument) {
+        return User.builder()
+                .id(userDocument.getId())
+                .name(userDocument.getName())
+                .build();
+    }
 }
--- phone-booking-restws/src/main/java/link/yauritux/phonebookingrestws/adapter/output/persistence/repository/UserRepository.java
+++ phone-booking-restws/src/main/java/link/yauritux/phonebookingrestws/adapter/output/persistence/repository/UserRepository.java
@@ -1,2 +1,33 @@
-package link.yauritux.phonebookingrestws.adapter.output.persistence.repository;public class UserRepository {
+package link.yauritux.phonebookingrestws.adapter.output.persistence.repository;
+
+import link.yauritux.domain.entity.User;
+import link.yauritux.phonebookingrestws.adapter.output.persistence.mapper.UserDocumentEntityMapper;
+import link.yauritux.port.output.repository.UserRepositoryPort;
+import link.yauritux.restfulapi.adapter.output.persistence.repository.UserDocumentRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Yauri Attamimi (yauritux@gmail.com)
+ * @version 1.0
+ *
+ * An adapter class used to perform any CRUD operation against our mongo database after some processes completed
+ * by the user domain service in the core domain layer.
+ *
+ */
+@RequiredArgsConstructor
+public class UserRepository implements UserRepositoryPort {
+
+    private final UserDocumentRepository userDocumentRepository;
+
+    @Override
+    public Mono<User> save(User user) {
+        return userDocumentRepository.save(UserDocumentEntityMapper.INSTANCE.entityToDocument(user))
+                .map(UserDocumentEntityMapper.INSTANCE::documentToEntity);
+    }
+
+    @Override
+    public Mono<User> findById(String id) {
+        return userDocumentRepository.findById(id).map(UserDocumentEntityMapper.INSTANCE::documentToEntity);
+    }
 }
