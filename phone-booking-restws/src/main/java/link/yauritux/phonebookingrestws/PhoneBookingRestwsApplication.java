3875ac50bc5c7f512af97dd5a13be5f5ff706b9d
Yauri Attamimi <yauritux@gmail.com>
2023-05-22 16:48:56 +0700

    add new module for restful api services

---
+++ phone-booking-restws/src/main/java/link/yauritux/phonebookingrestws/PhoneBookingRestwsApplication.java
@@ -0,0 +1,13 @@
+package link.yauritux.phonebookingrestws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookingRestwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneBookingRestwsApplication.class, args);
+    }
+
+}
