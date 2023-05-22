b779cf323d748ea7b0ef7721c956ca39bbacca20
Yauri Attamimi <yauritux@gmail.com>
2023-05-22 15:58:45 +0700

    initialize Phone business entity

--- phone-booking-core/src/main/java/link/yauritux/domain/entity/Phone.java
+++ phone-booking-core/src/main/java/link/yauritux/domain/entity/Phone.java
@@ -1,2 +1,45 @@
-package link.yauritux.domain.entity;public class Phone {
+package link.yauritux.domain.entity;
+
+import link.yauritux.exception.PhoneRegistrationFailedException;
+import link.yauritux.exception.UserRegistrationFailedException;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Yauri Attamimi (yauritux@gmail.com)
+ * @version 1.0
+ *
+ * Represents a Phone business entity.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Phone {
+
+    private String id;
+    private String brand;
+    private String deviceName;
+    private String technology;
+    private String _2gBands;
+    private String _3gBands;
+    private String _4gBands;
+    private boolean available;
+
+    /**
+     * This method will validate all entries necessary before a phone is registered into the system.
+     *
+     * @throws PhoneRegistrationFailedException will be thrown by the system
+     * in case one or more business validations failed to be fulfilled.
+     */
+    public void validateEntity() throws PhoneRegistrationFailedException {
+        if (brand == null || brand.trim().equals("")) {
+            throw new PhoneRegistrationFailedException("brand must not be empty!");
+        }
+        if (deviceName == null || deviceName.trim().equals("")) {
+            throw new PhoneRegistrationFailedException("device name must not be empty!");
+        }
+    }
 }
--- phone-booking-core/src/main/java/link/yauritux/exception/PhoneRegistrationFailedException.java
+++ phone-booking-core/src/main/java/link/yauritux/exception/PhoneRegistrationFailedException.java
@@ -1,2 +1,12 @@
-package link.yauritux.exception;public class PhoneRegistrationFailedException {
+package link.yauritux.exception;
+
+/**
+ * @author Yauri Attamimi (yauritux@gmail.com)
+ * @version 1.0
+ */
+public class PhoneRegistrationFailedException extends RuntimeException {
+
+    public PhoneRegistrationFailedException(String message) {
+        super(message);
+    }
 }
