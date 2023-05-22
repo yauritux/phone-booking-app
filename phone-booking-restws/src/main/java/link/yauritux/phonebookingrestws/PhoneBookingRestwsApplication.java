3082efef33cd46968b8873bf10ad278c0bcb27d9
Yauri Attamimi <yauritux@gmail.com>
2023-05-22 21:25:17 +0700

    load initial phone data

--- phone-booking-restws/src/main/java/link/yauritux/phonebookingrestws/PhoneBookingRestwsApplication.java
+++ phone-booking-restws/src/main/java/link/yauritux/phonebookingrestws/PhoneBookingRestwsApplication.java
@@ -3,10 +3,19 @@ package link.yauritux.phonebookingrestws;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import jakarta.annotation.PostConstruct;
+import link.yauritux.phonebookingrestws.adapter.output.persistence.document.PhoneDocument;
+import link.yauritux.phonebookingrestws.adapter.output.persistence.repository.PhoneDocumentRepository;
+import link.yauritux.port.output.repository.PhoneSpecRepositoryPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 /**
  * @author Yauri Attamimi (yauritux@gmail.com)
@@ -22,8 +31,40 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableReactiveMongoRepositories
 public class PhoneBookingRestwsApplication {
 
+    @Autowired
+    @Qualifier("fonoApiClient")
+    private PhoneSpecRepositoryPort fonoApiClient;
+
+    @Autowired
+    private PhoneDocumentRepository phoneDocumentRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(PhoneBookingRestwsApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        phoneDocumentRepository.deleteAll().subscribe();
+
+        Flux<String> brands = Flux.fromIterable(List.of("Samsung", "Samsung", "Motorola", "Oneplus", "Apple",
+                "Apple", "Apple", "Apple", "Nokia"));
+        Flux<String> models = Flux.fromIterable(List.of("Samsung Galaxy S9", "2x Samsung Galaxy S8",
+                "Motorola Nexus 6", "Oneplus 9", "Apple iPhone 13", "Apple iPhone 12", "Apple iPhone 11",
+                "iPhone X", "Nokia 3310"));
+
+        brands.zipWith(models).subscribe(p -> fonoApiClient.fetchPhoneSpecification(
+                p.getT1(), p.getT2()).subscribe(r -> {
+            var phone = PhoneDocument.builder()
+                    .brand(r.brand())
+                    .deviceName(r.deviceName())
+                    .technology(r.technology())
+                    ._2gBands(r._2gBands())
+                    ._3gBands(r._3gBands())
+                    ._4gBands(r._4gBands())
+                    .available(true)
+                    .build();
+            phoneDocumentRepository.save(phone).subscribe();
+        }));
     }
 
 }
